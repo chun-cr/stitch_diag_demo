e19a6862fe8ff02b48258f436432f8b5c25e5a80
Chun <3553680396@qq.com>
2026-04-15 11:56:07 +0800

    feat:"再次变更bundleid"

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,8 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.huaji.myapp.dev"
+//        applicationId = "com.huaji.myapp.dev"
+        applicationId = "com.permillet.myapp.dev"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 21
